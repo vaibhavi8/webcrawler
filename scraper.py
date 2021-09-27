b3458c7c19afab8962c683149232f40c393bf806
arushkakkar <arushkakr@gmail.com>
2021-09-27 13:54:44 -0700

    get doc

---
+++ scraper.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    seed = requests.get('https://www.espn.com/')
+    print(seed.text)
+
+    parser = BeautifulSoup(seed.text, 'html.parser')
+    print(parser.prettify)
+    
+if __name__ == '__main__':
+    main()
