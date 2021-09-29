d926ef7a81084cc44b1b5ebcc645ec50d7811223
vaibhavi8 <vaibjhawar@gmail.com>
2021-09-28 18:19:20 -0700

    updated the webcrawler
    webcrawler now finds all the links on the home page under the tag "quicklinks_list__links" and stores the in an array

--- scraper.py
+++ scraper.py
@@ -3,10 +3,13 @@ import requests
 
 def main():
     seed = requests.get('https://www.espn.com/')
-    print(seed.text)
+    '''print(seed.text)'''
 
-    parser = BeautifulSoup(seed.text, 'html.parser')
-    
+    parser = BeautifulSoup(seed.content, 'html.parser')
+    links = []
+    for i in parser.find_all('a', attrs={'class': 'quicklinks_list__link'}) :
+        links.append([i.text, i['href']])
+    print(links[0])
 
 if __name__ == '__main__':
     main()
