3e9919e027eab15761b09f8b19bedb20c773dda9
vaibhavi8 <vaibjhawar@gmail.com>
2021-09-28 18:56:57 -0700

    writes links to csv file

--- scraper.py
+++ scraper.py
@@ -1,15 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
+import csv 
 
 def main():
+    f = open('./repository/report.csv', 'w')
+    writer = csv.writer(f)
+
     seed = requests.get('https://www.espn.com/')
-    '''print(seed.text)'''
+    #print(seed.text)
 
     parser = BeautifulSoup(seed.content, 'html.parser')
     links = []
     for i in parser.find_all('a', attrs={'class': 'quicklinks_list__link'}) :
         links.append([i.text, i['href']])
+    writer.writerows(links)
+
     print(links[0])
+    f.close()
 
 if __name__ == '__main__':
     main()
